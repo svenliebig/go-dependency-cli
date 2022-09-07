7dfd0eb76abf93ec281425772c470cf2fdb07b99
Sven Liebig <sven.liebig@nortal.com>
2022-09-06 23:17:17 -0700

    so, it works, but i am basically cheating

--- internal/clone/clone.go
+++ internal/clone/clone.go
@@ -2,28 +2,166 @@ package clone
 
 import (
 	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
 
+	"github.com/go-git/go-billy/v5"
+	"github.com/go-git/go-billy/v5/memfs"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/storage/memory"
 )
 
+type FilesystemConverter interface {
+	Convert() fs.FS
+}
+
+type BillyFilesystemConverter struct {
+	from billy.Filesystem
+}
+
+type MyOwnFS struct {
+	files map[string]fs.File
+}
+
+type MyFile struct {
+	file billy.File
+	info fs.FileInfo
+}
+
+func (f MyFile) Stat() (fs.FileInfo, error) {
+	return f.info, nil
+}
+
+func (f MyFile) Read(b []byte) (int, error) {
+	return f.file.Read(b)
+}
+
+func (f MyFile) Close() error {
+	return f.file.Close()
+}
+
+func (myFs *MyOwnFS) Open(name string) (fs.File, error) {
+	file := myFs.files[name]
+
+	if file == nil {
+		return nil, &fs.PathError{Op: "Open", Path: name}
+	}
+
+	return file, nil
+}
+
+func (converter *BillyFilesystemConverter) Convert() fs.FS {
+	root := converter.from.Root()
+	infos, err := converter.from.ReadDir(root)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	files := converter.convertDirectory(infos)
+
+	repositoryFs := new(MyOwnFS)
+	repositoryFs.files = files
+
+	return repositoryFs
+}
+
+func (converter *BillyFilesystemConverter) convertDirectory(fileInfos []fs.FileInfo) map[string]fs.File {
+	m := map[string]fs.File{}
+
+	for index := range fileInfos {
+		fileInfo := fileInfos[index]
+
+		if fileInfo.IsDir() {
+			// cantina band
+		} else {
+			billyFile, err := converter.from.Open(fileInfo.Name())
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			var bytes []byte
+			_, err = billyFile.Read(bytes)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			file := new(MyFile)
+			file.file = billyFile
+			file.info = fileInfo
+
+			// billyFile.Close()
+
+			// No, Create does actually create a file in the filesystem
+			// osFile, err := os.Create(billyFile.Name())
+			// osFile := os.NewFile(0, billyFile.Name())
+
+			// if err != nil {
+			// 	log.Fatal(err)
+			// }
+			// osFile := os.NewFile(uintptr(length), billyFile.Name())
+
+			// _, err = osFile.Write(bytes)
+
+			// if err != nil {
+			// 	log.Fatal(err)
+			// }
+
+			// osFile.Close()
+
+			m[billyFile.Name()] = file
+		}
+	}
+
+	return m
+}
+
 // TODO add branch parameter
 // TODO add FS return value
-func GitClone(url string) {
+func GitClone(url string, branch string) (fs.FS, error) {
 	memory := memory.NewStorage()
+	billyFs := memfs.New()
 
 	// TODO use this `nil` there, that should be a fileSystem
-	repository, err := git.Clone(memory, nil, &git.CloneOptions{
+	repository, err := git.Clone(memory, billyFs, &git.CloneOptions{
 		URL:      url,
 		Progress: os.Stdout,
 		// TODO this should be a parameter
 		// TODO decide if you want to go straight for the `main` branch or if
 		//      you want to check if the branch exists. With a nice help.
 		ReferenceName: "refs/heads/main",
+		// TODO evaluate for performance
+		SingleBranch: true,
 	})
+
+	fmt.Println(billyFs.Root())
+
+	converter := new(BillyFilesystemConverter)
+	converter.from = billyFs
+	newFs := converter.Convert()
+
+	fmt.Println(newFs)
+
+	// dirInfo, err := billyFs.ReadDir(billyFs.Root())
+
+	// for index := range dirInfo {
+	// 	nfo := dirInfo[index]
+
+	// 	if nfo.IsDir() {
+	// 		// cantina band
+	// 	} else {
+	// 		fmt.Println(nfo.Name())
+	// 		fmt.Println(nfo.Size())
+
+	// 	}
+	// }
+
+	// b, _ := repository.Branch("main")
+	// ref, _ := repository.Reference(plumbing.ReferenceName(b.Remote))
 
 	treeObjects, _ := repository.TreeObjects()
 
@@ -32,6 +170,7 @@ func GitClone(url string) {
 	for {
 		// TODO What are TreeObjects and why are there so many of them
 		//      The TreeObjects have the partly the same files.
+		//      https://stackoverflow.com/questions/54139971/what-does-a-tree-mean-in-git
 		treeObject, erro := treeObjects.Next()
 
 		if treeObject == nil {
@@ -119,14 +258,26 @@ func GitClone(url string) {
 		reader, _ := el.Reader()
 		objectType := el.Type()
 
+		// https://gobyexample.com/reading-files
+		// Q: What's the difference between this
 		data, _ := ioutil.ReadAll(reader)
+		str := string(data)
+
+		// Q: and this:
+		// objectBytes := make([]byte, el.Size())
+		// length, _ := reader.Read(objectBytes)
+		// str := string(objectBytes[:length])
+
 		reader.Close()
 
-		str := string(data)
 		fmt.Println(str) // <here
 		fmt.Println(objectType.String())
 		fmt.Println("Key:", key, "=>", "Element:", el)
 	}
 
 	log.Default().Println(config.Author.Email)
+
+	return newFs, nil
 }
+
+// TODO test what is fast, map or array search
--- internal/clone/clone_test.go
+++ internal/clone/clone_test.go
@@ -1,27 +1,47 @@
 package clone
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGitClone(t *testing.T) {
-	type args struct {
-		url string
-	}
 
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "Should clone the repository 'https://github.com/halimath/mini-httpd.git'",
-			args: args{
-				url: "https://github.com/halimath/mini-httpd.git",
-			},
-		},
-	}
+	t.Run("should clone the repository 'https://github.com/halimath/mini-httpd.git' and transform it into a fs.FS", func(t *testing.T) {
+		fs, err := GitClone("https://github.com/halimath/mini-httpd.git", "main")
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			GitClone(tt.args.url)
-		})
-	}
+		if err != nil {
+			t.Errorf("GitClone() error = %v, and we didn't ask for it", err)
+			return
+		}
+
+		file, fileOpenError := fs.Open("LICENSE")
+
+		if fileOpenError != nil {
+			t.Errorf("fs.Open() error = %v, and we didn't ask for it", fileOpenError)
+			return
+		}
+
+		stat, fileStatError := file.Stat()
+
+		if fileStatError != nil {
+			t.Errorf("file.Stat() error = %v, and we didn't ask for it", fileStatError)
+			return
+		}
+
+		// Q: What's the difference
+		fileBytes := make([]byte, stat.Size())
+		length, fileReadError := file.Read(fileBytes)
+
+		if fileStatError != nil {
+			t.Errorf("file.Read() error = %v, and we didn't ask for it", fileReadError)
+			return
+		}
+
+		// TODO I could just somehow io.ReadAtLeast() https://gobyexample.com/reading-files
+
+		// Q: What's the difference
+		fmt.Printf("%d bytes: %s\n", length, string(fileBytes[:length]))
+		fmt.Println(string(fileBytes))
+	})
 }
